0541a3a01c649df89881eca7618383df7fc98d25
Jadon Scholes <jadonscholes@gmail.com>
2021-07-14 11:15:17 -0600

    Got star wars data from api

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people")
+      .then((response) => setCharacters(response.data.results))
+      .catch((error) => "There was an error: " + error);
+  }, []);
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div className='App'>
+      <h1 className='Header'>Characters</h1>
+      {characters == null ? (
+        <div>Fetching the character data...</div>
+      ) : (
+        characters.map((character, index) => (
+          <div key={index}>
+            <Character info={character} />
+          </div>
+        ))
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import React from "react";
+import "./Character.css";
+
+function Character({ info }) {
+  return (
+    <div className='Card'>
+      <p>{info.name}</p>
+    </div>
+  );
+}
+
+export default Character;
